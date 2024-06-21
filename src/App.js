50a51101fcde43a688a9c70e5495b0bd5ad76a6f
Mapelujo <olamideiyanda18@gmail.com>
2024-06-21 21:48:28 +0100

    working on static parts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
+import iconStar from './images/icon-star.svg';
+
 export default function App() {
   return (
-    <div className="">
-      HELLO WORLD
+    <div className="bg-very-dark-blue h-screen">
+      <Rating />
     </div>
   );
+}
+
+function Rating() {
+  return (
+    <section className="absolute left-1/2 transform -translate-x-1/2 top-1/3 bg-dark-blue w-1/3 rounded-lg p-8">
+      <div className='bg-small-dark-blue rounded-full flex justify-center items-center w-12 h-12'>
+        <img src={iconStar} alt='star-icon'></img>
+      </div>
+      <h1 className='text-white'>How did we do?</h1>
+      <p className='text-light-grey'>
+      Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
+      </p>
+      <div className='flex justify-between mt-8'>
+        <RateNumbers>1</RateNumbers>
+        <RateNumbers>2</RateNumbers>
+        <RateNumbers>3</RateNumbers>
+        <RateNumbers>4</RateNumbers>
+        <RateNumbers>5</RateNumbers>
+      </div>
+      <button className='bg-orange py-2 rounded-xl w-full'>Submit</button>
+    </section>
+  )
+}
+
+function RateNumbers({children}) {
+  return (
+    <div className='bg-small-dark-blue flex justify-center items-center text-light-grey rounded-full w-12 h-12'>
+      {children}
+    </div>
+  )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-blue': 'hsl(213, 19%, 18%)',
+        'very-dark-blue': 'hsl(216, 12%, 8%)',
+        'orange': 'hsl(25, 97%, 53%)',
+        'light-grey': 'hsl(217, 12%, 63%)',
+        'small-dark-blue': 'hsl(213, 15%, 14%)',
+      },
+      backgroundColor: {
+        'dark-blue': 'hsl(213, 19%, 18%)',
+        'very-dark-blue': 'hsl(216, 12%, 8%)',
+        'orange': 'hsl(25, 97%, 53%)'
+      },
+      textColor: {
+        'light-grey': 'hsl(217, 12%, 63%)',
+      },
+      fontFamily: {
+        overpass: ['Overpass', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
